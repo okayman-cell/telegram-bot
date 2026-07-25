0a5999721877062a8a89e6483ccab4b133ad86bc
okayman-cell <lev855217@gmail.com>
2026-07-25 20:08:32 +0200

    Refactor bot to use Router for command handling

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message, ChatPermissions
 import asyncio
+dp.include_router(router)
+
 
 import os 
 TOKEN = os.getenv("TOKEN")

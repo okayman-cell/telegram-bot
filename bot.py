16dfb81172336de1d28d1ed50b13388785d74e48
okayman-cell <lev855217@gmail.com>
2026-07-25 18:51:26 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,87 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, ChatPermissions
+import asyncio
+
+import os 
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Бот запущен!")
+
+@dp.message(Command("mute"))
+async def mute(message: Message):
+    if not message.reply_to_message:
+        await message.answer("Ответь на сообщение пользователя.")
+        return
+
+    user = message.reply_to_message.from_user.id
+
+    await bot.restrict_chat_member(
+        chat_id=message.chat.id,
+        user_id=user,
+        permissions=ChatPermissions(
+            can_send_messages=False
+        )
+    )
+
+    await message.answer("🔇 Пользователь замучен.")
+
+@dp.message(Command("unmute"))
+async def unmute(message: Message):
+    if not message.reply_to_message:
+        await message.answer("Ответь на сообщение пользователя.")
+        return
+
+    user = message.reply_to_message.from_user.id
+
+    await bot.restrict_chat_member(
+        chat_id=message.chat.id,
+        user_id=user,
+        permissions=ChatPermissions(
+            can_send_messages=True,
+            can_send_photos=True,
+            can_send_videos=True,
+            can_send_documents=True,
+            can_send_other_messages=True
+        )
+    )
+
+    await message.answer("🔊 Пользователь размучен.")
+
+@dp.message(Command("ban"))
+async def ban(message: Message):
+    if not message.reply_to_message:
+        await message.answer("Ответь на сообщение пользователя.")
+        return
+
+    await bot.ban_chat_member(
+        message.chat.id,
+        message.reply_to_message.from_user.id
+    )
+
+    await message.answer("⛔ Пользователь забанен.")
+
+@dp.message(Command("kick"))
+async def kick(message: Message):
+    if not message.reply_to_message:
+        await message.answer("Ответь на сообщение пользователя.")
+        return
+
+    user = message.reply_to_message.from_user.id
+
+    await bot.ban_chat_member(message.chat.id, user)
+    await bot.unban_chat_member(message.chat.id, user)
+
+    await message.answer("👢 Пользователь кикнут.")
+
+async def main():
+    print("bot start")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
